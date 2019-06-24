f78522c75012b0025bce563ed9dce11387526af0
KadziGit <arkadiusz.kadziolka@atos.net>
2019-06-24 15:49:21 +0200

    This is my first

--- 05_User Input.py
+++ 05_User Input.py
@@ -1,2 +1,3 @@
 variable = input("What is your name?: ")
 print("Hello " + variable + " !")
+
---
+++ teest.py
@@ -0,0 +1,4 @@
+num1 = input("Please provide first number")
+num2 = input("Please provide second number")
+result2 = float(num1) + float(num2)
+print(result2)
