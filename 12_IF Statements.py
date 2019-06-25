09039410312d6af969efea67e71ded5e2da79775
KadziGit <arkadiusz.kadziolka@atos.net>
2019-06-25 12:32:37 +0200

    This is my first

---
+++ 12_IF Statements.py
@@ -0,0 +1,8 @@
+is_male = False
+is_tall = False
+
+if is_male or is_tall:
+    print("You are a male or tall or both")
+else:
+    print("You are NOT a male or tall")
+
---
+++ 12_if.py
@@ -0,0 +1,12 @@
+is_male = True
+is_tall = True
+
+if is_male and is_tall
+    print("You are a male and tall")
+elif is_male and not is_tall:
+    print("You are a mail but not tall")
+
+elif not is_male and is_tall:
+    print("You are not a male but are tall")
+else:
+    print("you are not a male and not tall")
